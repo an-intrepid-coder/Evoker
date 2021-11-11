b17595e2132f355114052aa2a39178a0d2108aea
Samuel Gibson <sgibber2018@gmail.com>
2021-11-11 13:48:54 -0800

    Actor.kt
    Implemented FloodWater Actor.

--- src/main/kotlin/Actor.kt
+++ src/main/kotlin/Actor.kt
@@ -1,3 +1,5 @@
+val floodWaterTimer = 20 // for now. This arbitrary value is subject to balance changes down the road.
+
 /**
  * Actors include not just creatures and the player, but any object which can be interacted
  * with or observed. The instances of Actor each only interact with a fraction of the base class's
@@ -6,7 +8,7 @@
 sealed class Actor(
     val name: String,
     var maxHealth: Int? = null,
-    val additionalDescriptionLines: List<String>? = null,
+    var additionalDescriptionLines: List<String>? = null,
     val isPlayer: Boolean = false,
     var hidden: Boolean = false,
     var locked: Boolean = false,
@@ -68,7 +70,7 @@ sealed class Actor(
             },
             when (additionalDescriptionLines) {
                 null -> null
-                else -> additionalDescriptionLines.joinToString("\n")
+                else -> additionalDescriptionLines!!.joinToString("\n")
             },
         )
         return descriptionLines
@@ -125,8 +127,6 @@ sealed class Actor(
         maxHealth = 10,
         inventory = mutableListOf()
     )
-
-    // TODO: More kinds of Actors with behavior functions.
 
     class WanderingGolem : Actor(
         name = "Golem",
@@ -210,7 +210,7 @@ sealed class Actor(
     class GolemFootprints(
         turnsUntilExpiration: Int,
         additionalDescriptionLines: List<String>,
-    ) : Actor (
+    ) : Actor(
         name = "Footprints",
         timer = turnsUntilExpiration,
         additionalDescriptionLines = additionalDescriptionLines,
@@ -219,6 +219,53 @@ sealed class Actor(
             self.timer = self.timer!! - 1
             if (self.timer!! <= 0) self.kill()
             messages
+        }
+    )
+
+    class FloodWater : Actor(
+        name = "Water",
+        additionalDescriptionLines = listOf("You see flood water."),
+        timer = floodWaterTimer,
+        behavior = { scene, self ->
+            /*
+                FloodWater instances cause the water level in a room to rise. They disappear when the flood source
+                is plugged and the water level is NONE. They spread FloodWater to neighboring Scenes.
+             */
+            val messages = mutableListOf<String>()
+
+            if (scene.waterLevel.waterLevelType == WaterLevel.WaterLevelType.NONE && !scene.floodSource) {
+                self.kill()
+                messages
+            } else {
+                if (scene.floodSource) {
+                    scene.actors.removeAll { it.name == "Footprints" }
+                    self.timer = self.timer!! - 1
+                    if (self.timer == 0) {
+                        scene.waterLevel = scene.waterLevel.increment()
+                        self.timer = floodWaterTimer
+                    }
+                }
+
+                self.additionalDescriptionLines = when (scene.waterLevel.waterLevelType) {
+                    // TODO: When water-level effects are implemented it will give additional lines for each level.
+                    //  Those lines will describe the effects to the player.
+                    WaterLevel.WaterLevelType.ANKLES -> listOf("The water is up to your ankles.")
+                    WaterLevel.WaterLevelType.KNEES -> listOf("The water is up to your knees.")
+                    WaterLevel.WaterLevelType.WAIST -> listOf("The water is up to your waist.")
+                    WaterLevel.WaterLevelType.CHEST-> listOf("The water is up to your chest.")
+                    WaterLevel.WaterLevelType.UNDERWATER-> listOf("You are submerged under water.")
+                    WaterLevel.WaterLevelType.NONE -> error("This should never happen!")
+                }
+
+                scene.neighbors().forEach { neighbor ->
+                    if (neighbor.waterLevel.waterLevelType == WaterLevel.WaterLevelType.NONE) {
+                        neighbor.waterLevel = neighbor.waterLevel.increment()
+                        neighbor.addActor(FloodWater())
+                    }
+                }
+
+                messages
+            }
         }
     )
 
