af6f8ebc90eeb66f7733f767423bc78337aa7db3
Samuel Gibson <sgibber2018@gmail.com>
2021-11-12 11:35:59 -0800

    Testing
    Implemented an autoPlay feature which will allow for some basic testing. I'm trying to get in the habit of running more tests.

--- src/main/kotlin/EvokerGame.kt
+++ src/main/kotlin/EvokerGame.kt
@@ -9,6 +9,9 @@ class EvokerGame {
         return player.isAlive() == false
     }
 
+    /**
+     * Plays the game!
+     */
     fun play() {
         while (!gameOver()) {
             sceneMap.activeScene?.let { activeScene ->
@@ -31,6 +34,39 @@ class EvokerGame {
 
                     val deadActors = activeScene.refreshActors()
                     // TODO: Some actors will cause effects or change into other kinds of Actors once destroyed.
+                }
+            }
+        }
+    }
+
+    /**
+     * The game plays itself! Doubles as a testing environment. For now the player will be passive,
+     * but I'll build in some test AIs for the player down the road. I may or may not implement a real test suite
+     * down the road. This will serve the purpose, for now.
+     *
+     * This function currently spawns a passive player, some golems, and some water and runs them for numTurns,
+     * ensuring that nothing will crash. Eventually it will be more elaborate.
+     */
+    fun autoPlay(
+        numTurns: Int,
+        playerBehavior: ((Scene, Actor) -> Command) = { scene, _ ->
+            // By default, it is a passive player.
+            Command("wait", scene.actors)
+        }
+    ) {
+        sceneMap.scenes.values.random().addActor(Actor.WanderingGolem())
+        sceneMap.scenes.values.random().addActor(Actor.WanderingGolem())
+        sceneMap.scenes.values.random().addActor(Actor.FloodWater())
+        sceneMap.scenes.values.random().addActor(Actor.FloodWater())
+        repeat (numTurns) {
+            while (!gameOver() && turn < numTurns) {
+                sceneMap.activeScene?.let { activeScene ->
+                    val player = activeScene.getPlayer() ?: error("Player not found in active scene.")
+                    turn++
+                    activeScene.handleInput(
+                        playerBehavior.invoke(activeScene, player)
+                    )
+                    sceneMap.behaviorCheck()
                 }
             }
         }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
-    EvokerGame().play()
+    //EvokerGame().play()
+    crashTest(1000, 100000)
+}
+
+/**
+ * A very simple test which makes sure that none of the emergent and procedural systems will cause a crash over time,
+ * if left to their own devices.
+ */
+fun crashTest(
+    numTests: Int,
+    numTurnsPerTest: Int
+) {
+    println("Testing...")
+    repeat (numTests) { EvokerGame().autoPlay(numTurnsPerTest) }
+    println("Test passed! No crashes.")
 }
