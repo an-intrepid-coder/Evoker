0cdf62fd724cfbb15948d47f4ab22bbce723ed8e
Samuel Gibson <sgibber2018@gmail.com>
2021-11-15 14:29:08 -0800

    MessageLog.kt
    Implemented a Message class so that the MessageLog is less reliant on strings.

--- src/main/kotlin/MessageLog.kt
+++ src/main/kotlin/MessageLog.kt
@@ -1,20 +1,35 @@
-class MessageLog {
-    var unreadMessages = mutableListOf("Welcome to Evoker!\n")
-    val readMessages = mutableListOf<String>()
+data class Message(
+    val id: Int,
+    val turnOf: Int,
+    val text: String,
+)
 
-    fun messageIn(msg: String) {
+class MessageLog {
+    var unreadMessages = mutableListOf<Message>()
+    val readMessages = mutableListOf<Message>()
+    var numMessages = 0
+
+    init {
+        unreadMessages.add(Message(numMessages++, 0, "Welcome to Evoker!"))
+    }
+
+    fun messageIn(msg: Message) {
         unreadMessages.add(msg)
     }
 
-    private fun messageOut(): String? {
+    fun messageIn(turn: Int, text: String) {
+        unreadMessages.add(Message(numMessages++, turn, text))
+    }
+
+    private fun messageOut(): Message? {
         return unreadMessages.removeFirstOrNull()?.let { msg ->
             readMessages.add(msg)
             msg
         }
     }
 
-    fun outputAll(): List<String> {
-        val messages = mutableListOf<String>()
+    fun outputAll(): List<Message> {
+        val messages = mutableListOf<Message>()
         while (unreadMessages.isNotEmpty()) {
             messageOut()?.let { messages.add(it) }
         }
